a2c423a86cbd9f3524da77d24e518c377e67f329
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-09-13 11:03:11 +0200

    [fix] inserted block's position + refactoring

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const getFirstNotSeenActivityElement = () => {
+const getLastSeenActivityBlock = () => {
   const lastVisitDate = new Date(Date.parse('2018-09-13T00:30:50.349Z'));
   const pageItems = document.querySelectorAll('relative-time');
 
   for (const item of pageItems) {
-    if (item._date < lastVisitDate)
+    if (lastVisitDate > item._date)
       return item.closest('.body').parentElement;
   }
 };
 
-const insertOldActivityBlockAfter = (element) => {
+const insertOldActivityTextBefore = (element) => {
   const div = document.createElement('div');
   div.textContent = 'Old activity ↓';
   div.style.margin = '1rem 0';
   div.style.textAlign = 'center';
   div.style.width = '100%';
 
-  // Insert our block BEFORE the NEXT element, which is after the one we've selected
-  element.parentNode.insertBefore(div, element.nextSibling);
   // Remove previous element's bottom border
-  element.querySelector('.d-flex').classList.remove('border-bottom');
+  element.previousElementSibling.querySelector('.d-flex').classList.remove('border-bottom');
+
+  // Insert our block before the last seen activity block
+  element.parentNode.insertBefore(div, element);
 };
 
-const firstNotSeenActivityElement = getFirstNotSeenActivityElement();
-insertOldActivityBlockAfter(firstNotSeenActivityElement);
+const lastSeenActivityElement = getLastSeenActivityBlock();
+insertOldActivityTextBefore(lastSeenActivityElement);
