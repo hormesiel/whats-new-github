82e002b77c6a9214d2089c7cd22dc6fd09e4df56
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-09-13 11:10:37 +0200

    [fix] Handle case where all displayed activities have already been seen

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const getLastSeenActivityBlock = () => {
-  const lastVisitDate = new Date(Date.parse('2017-09-13T00:30:50.349Z'));
+  const lastVisitDate = new Date();
   const pageItems = document.querySelectorAll('relative-time');
 
   for (const item of pageItems) {
@@ -16,13 +16,16 @@ const insertOldActivityTextBefore = (element) => {
   div.style.width = '100%';
 
   // Remove previous element's bottom border
-  element.previousElementSibling.querySelector('.d-flex').classList.remove('border-bottom');
+  const previousElementWithBottomBorder = element.previousElementSibling.querySelector('.d-flex');
+  if (previousElementWithBottomBorder)
+    previousElementWithBottomBorder.classList.remove('border-bottom');
 
   // Insert our block before the last seen activity block
   element.parentNode.insertBefore(div, element);
 };
 
 const lastSeenActivityElement = getLastSeenActivityBlock();
+console.log(lastSeenActivityElement);
 
 // If not all activities displayed are new
 // which means there's at least one element the user has already seen
